6add8c79f551c0f6deadb8797600b8b0ac9296d7
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-17 13:04:20 +0200

    first screen ready

--- app/src/main/java/com/example/simplenavigation/MainActivity.kt
+++ app/src/main/java/com/example/simplenavigation/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.simplenavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simplenavigation.ui.theme.SimpleNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,3 +41,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun FirstScreen(){
+    val name = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("This is the First Screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value=name.value, onValueChange = {
+            name.value = it
+        })
+        Button(onClick = {
+
+        }) {
+            Text("Go to Second Screen")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstScreenPreview(){
+    FirstScreen()
+}
